caa7b2b2b498111331552f0bbfadc9cb6a660e1e
giuliamari04 <giuliamari04@gmail.com>
2023-11-13 15:59:49 +0100

    struttura,logica e grafica base

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,56 @@
+
 <template>
   <div>
-
+    <HeaderComponent />
+    <main>
+      <div class="container d-flex flex-wrap p-0 m-auto ">
+        <div v-for="card in store.cardList" :key="card.id" class="my-w">
+             <CardsComponent 
+            :img="card.card_images[0].image_url_small"
+            :name="card.name"
+            :species="card.archetype"
+          />
+          </div>
+         
+        </div>
+    </main>
   </div>
 </template>
-
-<script>
-  export default {
-    
-  }
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+  
+ <script>
+ import { store } from './components/data/store';
+ 
+ import HeaderComponent from './components/HeaderComponent.vue';
+ import CardsComponent from './components/CardsComponent.vue';
+ import axios from 'axios';
+ export default {
+   name: 'App',
+   components: {
+     HeaderComponent,
+     CardsComponent,
+   },
+   data() {
+     return {
+       store,
+     };
+   },
+   methods: {
+     getCards() {
+       axios.get(store.apiUrl).then((response) => {
+        //console.log(response);
+      store.cardList = response.data.data;
+       });
+     },
+   },
+   created() {
+     this.getCards();
+   }
+ };
+ </script>
+ 
+ <style lang="scss" scoped>
+ .container{
+  background-color: blue;
+ }
+ </style>
+ 
---
+++ src/components/CardsComponent.vue
@@ -0,0 +1,33 @@
+<template>
+            <div class="card">
+               <img :src="img" class="card-img-top" :alt="name">
+               <div class="card-body text-center ">
+                 <h5 class="card-title">{{ name }}</h5>
+                 <h5 class="card-title">{{ species }}</h5>
+               </div>
+             </div>
+   </template>
+   
+   <script>
+   
+       export default {
+         name:'CardsComponent',
+         props:{
+           img:String,
+           name:String,
+           species:String,
+         },
+           data(){
+               return{
+                
+               }
+           },
+          
+       }
+   </script>
+   
+   <style lang="scss" scoped>
+   .card{
+    background-color: red;
+   }
+   </style>
